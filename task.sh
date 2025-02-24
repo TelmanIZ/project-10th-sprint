1cfd106b46c32dbec6b9b6f7f729faab70e9386b
TelmanIZ <iskenderov.telman.98.gmail.com>
2025-02-24 23:56:46 +0300

    first commit

---
+++ task.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+echo './dir1/summary.txt
+./dir2/list.txt
+./dir3/dir4/list.txt
+empty
+hello.sh
+list.txt!
+в�Всем студентам, привет!
+./dir1/summary.txt
+./dir2/list.txt
+./dir3/dir4/list.txt '
